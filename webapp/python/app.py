9e999e0faf566378aa061a724f048685ac533eb4
Yosuke Otosu <y-otosu@boostry.co.jp>
2023-09-16 02:36:13 +0900

    fix app

--- webapp/python/app.py
+++ webapp/python/app.py
@@ -89,13 +89,18 @@ def get_initialize():
 
 
 def db_get_user(cur, user_id):
-    cur.execute("SELECT * FROM user WHERE id = %s", (user_id,))
+    cur.execute("SELECT * FROM user WHERE id = %s LIMIT 1", (user_id,))
     return cur.fetchone()
 
 
 def db_add_message(cur, channel_id, user_id, content):
     cur.execute("INSERT INTO message (channel_id, user_id, content, created_at) VALUES (%s, %s, %s, NOW())",
                 (channel_id, user_id, content))
+
+
+def db_is_exist_user(cur, user_id):
+    cur.execute("SELECT user.id FROM user WHERE id = %s LIMIT 1", (user_id,))
+    return cur.fetchone()
 
 
 def login_required(func):
@@ -184,7 +189,7 @@ def get_login():
 def post_login():
     name = flask.request.form['name']
     cur = dbh().cursor()
-    cur.execute("SELECT id, password, salt FROM user WHERE name = %s", (name,))
+    cur.execute("SELECT id, password, salt FROM user WHERE name = %s LIMIT 1", (name,))
     row = cur.fetchone()
     if not row or row['password'] != hashlib.sha1(
         (row['salt'] + flask.request.form['password']).encode('utf-8')
@@ -222,8 +227,8 @@ def get_message():
     last_message_id = int(flask.request.args.get('last_message_id'))
     cur = dbh().cursor()
     cur.execute(
-        "SELECT message.id, message.created_at, message.content, message.user_id FROM message WHERE id > %s AND channel_id = %s ORDER BY id DESC LIMIT 100",
-        (last_message_id, channel_id)
+        "SELECT message.id, message.created_at, message.content, message.user_id FROM message WHERE channel_id = %s AND id > %s ORDER BY id DESC LIMIT 100",
+        (channel_id, last_message_id)
     )
     rows = cur.fetchall()
     response = []
@@ -234,7 +239,7 @@ def get_message():
         r['id'] = row['id']
         user = user_dict.get(row['user_id'])
         if user is None:
-            cur.execute("SELECT name, display_name, avatar_icon FROM user WHERE id = %s", (row['user_id'],))
+            cur.execute("SELECT name, display_name, avatar_icon FROM user WHERE id = %s LIMIT 1", (row['user_id'],))
             user = cur.fetchone()
         r['user'] = user
         r['date'] = row['created_at'].strftime("%Y/%m/%d %H:%M:%S")
@@ -265,7 +270,7 @@ def fetch_unread():
 
     res = []
     for channel_id in channel_ids:
-        cur.execute('SELECT * FROM haveread WHERE user_id = %s AND channel_id = %s', (user_id, channel_id))
+        cur.execute('SELECT haveread.message_id FROM haveread WHERE user_id = %s AND channel_id = %s LIMIT 1', (user_id, channel_id))
         row = cur.fetchone()
         if row:
             cur.execute('SELECT COUNT(*) as cnt FROM message WHERE channel_id = %s AND %s < id',
@@ -331,7 +336,7 @@ def get_profile(user_name):
     channels, _ = get_channel_list_info()
 
     cur = dbh().cursor()
-    cur.execute("SELECT * FROM user WHERE name = %s", (user_name,))
+    cur.execute("SELECT * FROM user WHERE name = %s LIMIT 1", (user_name,))
     user = cur.fetchone()
 
     if not user:
@@ -423,12 +428,12 @@ def ext2mime(ext):
 @app.route('/icons/<file_name>')
 def get_icon(file_name):
     cur = dbh().cursor()
-    cur.execute("SELECT * FROM image WHERE name = %s", (file_name,))
+    cur.execute("SELECT image.id FROM image WHERE name = %s LIMIT 1", (file_name,))
     row = cur.fetchone()
     ext = os.path.splitext(file_name)[1] if '.' in file_name else ''
     mime = ext2mime(ext)
     if row:
-        cur.execute("SELECT * FROM image_data WHERE id = %s", (row["id"],))
+        cur.execute("SELECT image_data.data FROM image_data WHERE id = %s LIMIT 1", (row["id"],))
         image_data_row = cur.fetchone()
         if image_data_row:
             return flask.Response(image_data_row['data'], mimetype=mime)
